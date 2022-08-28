25aa64f7c5da358b4c28e356bffed405394595da
allisonvinicius <allisonvinicius63@gmail.com>
2022-08-28 14:32:22 -0400

    feat: include types files retirn

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 import usersReducer from "./features/UserSlice/UserSlice";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     users: usersReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+
 export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export default store;
