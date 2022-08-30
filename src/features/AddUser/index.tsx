8ba9687bf447d2c2b88aa4e6574b0b92b1fc4236
allisonvinicius <allisonvinicius63@gmail.com>
2022-08-30 00:08:50 -0400

    refactory: created object initialValues users

--- src/features/AddUser/index.tsx
+++ src/features/AddUser/index.tsx
@@ -2,18 +2,39 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Content from "../../components/Content";
 import { RootState } from "../../store";
-import { UserNameType, UserType } from "../../ts";
-import { addUsersAsync } from "../UserSlice/UserSlice";
+import { UserType } from "../../ts";
+import { addUser } from "../UserSlice/UserSlice";
 import { Button, Column, Container, Input, Row } from "./styles";
+
+const initialStateUser: UserType = {
+  email: "",
+  userName: "",
+  password: "",
+  id: 0,
+  name: {
+    firstname: "",
+    lastname: "",
+  },
+  address: {
+    city: "",
+    street: "",
+    number: 0,
+    zipcode: "",
+    geolocation: {
+      lat: "",
+      long: "",
+    },
+  },
+  phone: "",
+};
 
 const AddUser = () => {
   const user = useSelector((state: RootState) => state.users);
   const dispatch = useDispatch();
-  const [newUser, setNewUser] = useState<UserType>();
-  const [name, setName] = useState<UserNameType>();
+  const [values, setValues] = useState(initialStateUser);
 
   const handleSubmit = () => {
-    dispatch(addUsersAsync(newUser));
+    dispatch(addUser(values));
   };
 
   return (
@@ -24,64 +45,69 @@ const AddUser = () => {
             <Input
               type="text"
               placeholder="Digite o nome.."
-              onChange={e => setName({ ...name, name: e.target.value })}
+              value={values.name.firstname}
+              onChange={e =>
+                setValues({ ...values, firstname: e.target.value })
+              }
             />
 
             <Input
               type="text"
               placeholder="Sobrenome"
-              onChange={e => setNewUser({ ...newUser, name: e.target.value })}
+              value={values.name.lastname}
+              onChange={e => setValues({ ...values, name: e.target.value })}
             />
             <Input
               type="text"
               placeholder="Digite seu Email"
-              value={""}
-              onChange={e => setNewUser({ ...newUser, email: e.target.value })}
+              value={values.email}
+              onChange={e => setValues({ ...values, email: e.target.value })}
             />
             <Input
               type="text"
               placeholder="Nome de Usuário"
-              onChange={e =>
-                setNewUser({ ...newUser, username: e.target.value })
-              }
+              value={values.userName}
+              onChange={e => setValues({ ...values, username: e.target.value })}
             />
             <Input
               type="text"
               placeholder="Password"
-              onChange={e =>
-                setNewUser({ ...newUser, password: e.target.value })
-              }
+              value={values.password}
+              onChange={e => setValues({ ...values, password: e.target.value })}
             />
           </Column>
           <Column>
             <Input
               type="text"
               placeholder="Cidade"
-              onChange={e => setNewUser({ ...newUser, city: e.target.value })}
+              value={values.address.city}
+              onChange={e => setValues({ ...values, city: e.target.value })}
             />
 
             <Input
               type="text"
               placeholder="Rua"
-              onChange={e => setNewUser({ ...newUser, street: e.target.value })}
+              value={values.address.street}
+              onChange={e => setValues({ ...values, street: e.target.value })}
             />
 
             <Input
               type="text"
               placeholder="Numero"
-              onChange={e => setNewUser({ ...newUser, number: e.target.value })}
+              value={values.address.number}
+              onChange={e => setValues({ ...values, number: e.target.value })}
             />
             <Input
               type="text"
               placeholder="Telefone"
-              onChange={e => setNewUser({ ...newUser, phone: e.target.value })}
+              value={values.phone}
+              onChange={e => setValues({ ...values, phone: e.target.value })}
             />
             <Input
               type="text"
               placeholder="ZipCode"
-              onChange={e =>
-                setNewUser({ ...newUser, zipcode: e.target.value })
-              }
+              value={values.address.zipcode}
+              onChange={e => setValues({ ...values, zipcode: e.target.value })}
             />
           </Column>
         </Row>
