a6677ebe1519470ad8ee66637618f70df006a77d
allisonvinicius <allisonvinicius63@gmail.com>
2022-09-09 20:40:48 -0400

    refactory: inclde styles input

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.input`
+  justify-content: center;
+  width: 90%;
+  font-size: 16px;
+  padding: 25px;
+  border: none;
+  background-color: #d7dbdd;
+  border-radius: 8px;
+  margin: 10px;
+`;
