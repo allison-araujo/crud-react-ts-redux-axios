9ecb6f5453a3d811f6e9bd05002680d83b27e2c0
allisonvinicius <allisonvinicius63@gmail.com>
2022-09-14 00:57:19 -0400

    refactory: refactory component list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,63 +1,35 @@
-import { Container, Table, Tbody, Td, Th, Thead, Tr } from "./styles";
+import { UserType } from "../../ts";
+import TableUser from "../TableUser";
+import { Tbody } from "./styles";
 
 type User = {
   user: [];
-  isDelete: boolean;
 };
 
-interface IUserProps {
-  firstName: string;
-  lastName: string;
-  phone: string;
-  city: string;
-  street?: string;
-  number?: number;
-  zipcode?: string;
-  lat?: string;
-  long?: string;
-  id?: number;
-  email: string;
-  userName?: string;
-  password?: string;
-}
-
-const List = ({ user, isDelete }: User) => {
+const List = ({ user }: User) => {
   return (
     <>
-      <Container>
-        <Table>
-          {isDelete ? (
-            <Thead>
-              <Tr>
-                <Th>Name</Th>
-                <Th>Sobrenome</Th>
-                <Th>Email</Th>
-              </Tr>
-            </Thead>
-          ) : (
-            <Thead>
-              <Tr>
-                <Th>Name</Th>
-                <Th>Sobrenome</Th>
-                <Th>Email</Th>
-                <Th>Fone</Th>
-                <Th>Delete</Th>
-              </Tr>
-            </Thead>
-          )}
-
-          {user.map((item: IUserProps) => (
-            <Tbody>
-              <Tr key={item.id}>
-                <Td>{item.firstName}</Td>
-                <Td>{item.lastName}</Td>
-                <Td>{item.email}</Td>
-                <Td>{item.phone}</Td>
-              </Tr>
-            </Tbody>
-          ))}
-        </Table>
-      </Container>
+      {user &&
+        user.map((item: UserType) => (
+          <Tbody>
+            <TableUser
+              key={item.id}
+              id={item.id}
+              firstname={item.name.firstname}
+              lastname={item.name.lastname}
+              email={item.email}
+              phone={item.phone}
+              city={item.address.city}
+              number={item.address.number}
+              username={item.username}
+              lat={item.address.geolocation.lat}
+              long={item.address.geolocation.long}
+              street={item.address.street}
+              password={item.password}
+              zipcode={item.address.zipcode}
+            />
+          </Tbody>
+        ))}
     </>
   );
 };
